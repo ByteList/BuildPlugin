64c256738a1d2f33417a716f2d17813fb909d2ae
ByteList <niklas.bytelist@gmail.com>
2018-01-15 16:30:16 +0100

    Fix motd error.

--- src/main/java/de/gamechest/buildplugin/listener/ServerPingListener.java
+++ src/main/java/de/gamechest/buildplugin/listener/ServerPingListener.java
@@ -13,6 +13,6 @@ public class ServerPingListener implements Listener {
 
     @EventHandler
     public void onServerPing(ServerListPingEvent e) {
-        e.setMotd("§6Game-Chest§f.§6de §7\u00BB §eBuild-Server §8[§b1.9 §f- §c1.12§8]\n§aThe future is now, old men!");
+        e.setMotd("§6Game-Chest§f.§6de §7\u00BB §eBuild-Server §8[§b1.9 §f- §c1.12§8]\n§aThe future is now, old man!");
     }
 }
